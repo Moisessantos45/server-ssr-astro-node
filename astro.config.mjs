cd1a06050efdb47cfac48f4c2ae947fa7ad60690
Moisessantos45 <moises.santos.toman@gmail.com>
2024-08-02 17:53:28 -0600

    three commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), node()],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
 });
